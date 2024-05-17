a201dd38e751c9f405fa0d138793cdbee5dec46a
GabrielCancro3OGS <119443886+GabrielCancro3OGS@users.noreply.github.com>
2024-05-16 22:16:35 -0300

    update

--- autoloads/itemManager.gd
+++ autoloads/itemManager.gd
@@ -6,8 +6,15 @@ var items = {
 	"potion1":{"name":"potion","type":"use","ico":"potion","dices":["SW"]},
 }
 
+var dices = {
+	"dBS":["SW","SW","HN","BT","",""]
+}
+
 func get_item_data(item_code):
 	var it = items[item_code].duplicate()
 	it["code"] = item_code
 	if it["type"]=="equip": it["equipped"] = false
 	return it
+
+func get_dice(dice_code):
+	return dices[dice_code]
